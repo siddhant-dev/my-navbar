365cbc90c9cb5292f9a8bec469bfcd5dbabe3d79
Siddhant Jaiswal <34509466+Hades24@users.noreply.github.com>
2019-11-15 18:44:03 +0530

    Added Animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { ModalService } from 'angular-modal-library';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practise';
+  showMenu = false;
+  element: any;
+
+
+  ngOnInit() {
+    this.element = document.querySelector('.modal-body');
+    this.element.style.borderRadius = '15px';
+    console.log(this.element.classList);
+
+  }
+
+  menu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  constructor(private modal: ModalService, private el: ElementRef) {
+    this.element = el.nativeElement;
+  }
+
+  open(id: string) {
+    this.modal.open(id);
+
+  }
+  close(id: string) {
+    this.modal.close(id);
+  }
+  
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ModalModule } from 'angular-modal-library';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ModalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
